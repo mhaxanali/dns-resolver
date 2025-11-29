221d0b113b08a15fd2c73a64b7ab7ca469940429
Muhammad Hasan <mhasanali2010@gmail.com>
2025-11-29 12:45:25 +0500

    add script.js

---
+++ frontend/script.js
@@ -0,0 +1,21 @@
+const resultsPlaceholder = document.getElementById("results-placeholder")
+const input = document.getElementById("domain-input")
+const btn = document.getElementById("resolve-btn")
+
+btn.addEventListener("click", () => {
+    const domain = input.value  // Read value when clicked
+    resolve(domain)
+})
+
+function resolve(domain) {
+    const container = document.getElementById("results-container")
+    const placeholder = document.getElementById("results-placeholder")
+    
+    placeholder.hidden = true  // Hide placeholder
+    
+    fetch(`http://localhost:8080/lookup?domain=${domain}`)
+        .then(res => res.json())
+        .then(data => {
+            container.innerHTML = `<p style="color: #00e0ff; font-size: 1.5rem;">IP: ${data.ip}</p>`
+        })
+}
