25fe0bb345cf9c4d149bdcb6447774aa14dd36e3
Muhammad Hasan <mhasanali2010@gmail.com>
2025-11-29 11:17:20 +0500

    add log

--- backend/flask-server/server.py
+++ backend/flask-server/server.py
@@ -56,4 +56,5 @@ def store():
     return jsonify({"status":"success"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    log.info("Service started.")
+    app.run(host="0.0.0.0", port=5050, debug=False)
